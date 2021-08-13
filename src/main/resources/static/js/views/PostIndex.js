841a852a328cdcef94f6204959e194809749b59b
shawcorey <shaw.corey@gmail.com>
2021-08-13 12:14:41 -0500

    updated file

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,10 +1,25 @@
+import fetchData from "../fetchData.js";
+
 export default function PostIndex(props) {
     return `
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
-<!--MAKE CREATE FORM HERE-->
+<!--MAKE CREATE FORM HERE--> 
+       <form>
+
+    <label for="username">Username</label>
+    <input id="username" name="username" type="text">
+    <br>
+
+    <label for="password">Password</label>
+    <input id="password" name="password" type="password">
+    <br>
+
+    <input type="button" id="create-post-btn">
+</form>
+            
             <div class="post-container">
                 ${props.posts.map(post =>
         `
@@ -13,14 +28,36 @@ export default function PostIndex(props) {
        <!--add edit, delete buttons, add edit form-->
         
         
-        `                  ).join('')}   
+        `).join('')}   
             </div>
             
             
         </main>
     `;
 }
+//add event listeners, get data, send fetch
+//call function for edit btns listener
+//call function for data
+//call function for fetch
+export function PostsEvent() {
+    $('#create-post-btn').click(function () {
 
-export function PostsEvent(){
-    //add event listeners, get data, send fetch
+        let post = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        }
+        console.log(post)
+
+
+        let request = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(post)
+        }
+        fetchData({
+                post: '/api/posts'
+            },
+            request)
+
+    });
 }
