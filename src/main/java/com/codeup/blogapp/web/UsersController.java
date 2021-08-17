7a369bd3369a11df7aae569110b65e659d4dd665
shawcorey <shaw.corey@gmail.com>
2021-08-17 11:54:15 -0500

    updated blogapp

--- src/main/java/com/codeup/blogapp/web/UsersController.java
+++ src/main/java/com/codeup/blogapp/web/UsersController.java
@@ -3,47 +3,73 @@ package com.codeup.blogapp.web;
 import com.codeup.blogapp.data.User;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/users")
 public class UsersController {
-private List<User> getUser(){
-    return new ArrayList<>(){{
-        add(new User(1l,"User", "new.email@gmail.com", "1234qwerty"));
-    }};
+    private List<User> getUser() {
+        return new ArrayList<>() {{
+            add(new User(1l, "User", "new.email@gmail.com", "1234qwerty"));
+        }};
 
 
-}
+    }
 
     @GetMapping("{id}")
-    private User getUserById(@PathVariable Long id){
+    private User getUserById(@PathVariable Long id) {
 
-      if (id == 1){
+        if (id == 1) {
 
-          return new User(1L,"User","email@email.com",
-                  "qwasdf1234");
-      }else {
-          return null;
-      }
+            return new User(1L, "User", "email@email.com",
+                    "qwasdf1234");
+        } else {
+            return null;
+        }
     }
 
     @PostMapping
-    private void createUser(@RequestBody User newUser){
+    private void createUser(@RequestBody User newUser) {
         System.out.println(newUser.getEmail());
         System.out.println(newUser.getUsername());
         System.out.println(newUser.getPassword());
     }
+
     @PutMapping("/{id}")
-    private void updateUser(@PathVariable Long id, @RequestBody User user){
+    private void updateUser(@PathVariable Long id, @RequestBody User user) {
         System.out.println(id);
         System.out.println(user.getEmail());
         System.out.println(user.getUsername());
         System.out.println(user.getPassword());
     }
+
     @DeleteMapping({"/{id}"})
-    private void deleteUser(@PathVariable Long id){
+    private void deleteUser(@PathVariable Long id) {
         System.out.println("Deleting User with ID: " + id);
+    }
+
+    @GetMapping("{id}")
+    private User findByID(@PathVariable Long id) {
+        return new User(1L, "User", "email@email.com", "password");
+        }
+    }
+
+    @GetMapping("/findByUsername")
+    private void findByUsername(@RequestParam String username) {
+        return new User;
+    }
+
+    @GetMapping("findByEmail")
+    private void findByEmail(@RequestParam String email) {
+        return new User;
+    }
+
+
+    @GetMapping("{id}/updatePassword")
+    private void updatePassword(@PathVariable Long id, @RequestParam(required = false) String oldPassword, @Valid @Size(min = 3) @RequestParam String newPassword) {
+
     }
 }
