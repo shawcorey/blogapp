8cd06d150e849ac88709c6fe0f27efad2c0ca47b
shawcorey <shaw.corey@gmail.com>
2021-08-18 22:18:09 -0500

    updated blogapp with CategoriesController

--- src/main/java/com/codeup/blogapp/web/CategoriesController.java
+++ src/main/java/com/codeup/blogapp/web/CategoriesController.java
@@ -1,5 +1,15 @@
 package com.codeup.blogapp.web;
 
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 public class CategoriesController {
+
+    private Object Category;
+
+    private Object getPostsByCategory(@RequestParam String categoryName){
+        return Category;
+    }
+
 
 }
