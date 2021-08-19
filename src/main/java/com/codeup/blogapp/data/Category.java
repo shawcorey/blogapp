a11bc7924ecc6ef7ad5a17546f0607f516687144
shawcorey <shaw.corey@gmail.com>
2021-08-19 10:49:13 -0500

    update blogapp

--- src/main/java/com/codeup/blogapp/data/Category.java
+++ src/main/java/com/codeup/blogapp/data/Category.java
@@ -5,6 +5,8 @@ public class Category {
     private String name;
     private Post post;
 
+    public Category() {
+    }
 
     public Category(Long id, String name, Post post) {
         this.id = id;
--- src/main/java/com/codeup/blogapp/web/CategoriesController.java
+++ src/main/java/com/codeup/blogapp/web/CategoriesController.java
@@ -1,15 +1,35 @@
 package com.codeup.blogapp.web;
 
+import com.codeup.blogapp.data.Category;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 
-public class CategoriesController {
+@RestController
+@RequestMapping(value="/api/categories", headers = "application/json")
+  public class CategoriesController {
+  public CategoriesController() {
+  }
 
-    private Object Category;
+  @GetMapping
+    private List<Category> getCategories(){
+        return null;
 
-    private Object getPostsByCategory(@RequestParam String categoryName){
-        return Category;
     }
 
-
 }
+//public class CategoriesController {
+//
+//    private Object Category;
+//
+//    private Object getPostsByCategory(@RequestParam String categoryName){
+//        return Category;
+//    }
+//
+//    public CategoriesController(Object category) {
+//        Category = category;
+//    }
+//}
