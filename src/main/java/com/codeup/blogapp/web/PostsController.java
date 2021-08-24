64f81096b3f24ea6b51b553b347d26f9d8cd4c26
shawcorey <shaw.corey@gmail.com>
2021-08-24 09:05:27 -0500

    updated blogapp

--- src/main/java/com/codeup/blogapp/web/PostsController.java
+++ src/main/java/com/codeup/blogapp/web/PostsController.java
@@ -3,6 +3,7 @@ package com.codeup.blogapp.web;
 
 import com.codeup.blogapp.data.Category;
 import com.codeup.blogapp.data.Post;
+import com.codeup.blogapp.data.PostsRepository;
 import com.codeup.blogapp.data.User;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,54 +18,45 @@ import java.util.List;
 // we are informing Spring of how to direct requests to PostsController,
 // as well as what type of data is accepted.
 public class PostsController {
-    Collection<Category> listCategory = new ArrayList<>(){{
-        add(new Category(1L,"Category"));
-    }};
+    private final PostsRepository postsRepository;
+
+    public PostsController(PostsRepository postsRepository) {
+        this.postsRepository = postsRepository;
+    }
+
     @GetMapping
     //This handy annotation is what is called a composed annotation.
     // @GetMapping is a composed annotation that acts as a shortcut for
     // @RequestMapping(method = RequestMethod.GET).
-    private List<Post> getPost() {
+    private List<Post> getPosts() {
 
-        User user = new User(1L,"coco","thy@email.com","pwd");
-
-        return new ArrayList<>() {{
-            add(new Post(1L, "A new Post",
-                    "this is a brilliant post. 10/10", user, listCategory));
-            add(new Post(2L, "a newer post",
-                    "this is slightly longer. 11/10", user, listCategory));
-            add(new Post(3L, "A new post",
-                    "this is even longer. 12/10", user, listCategory));
-        }};
+        return postsRepository.findAll();
     }
 
 
     @GetMapping("{id}")
     private Post getPostById(@PathVariable Long id) {
         // /api/posts/1
-        if (id == 1) {
-
-            return new Post(1L, "A new Post",
-                    "this is a brilliant post. 10/10", new User("coco"), listCategory);
-        } else {
-            return null;
-        }
+      return postsRepository.findById(id).get();
     }
 
     @PostMapping
     private void createPost(@RequestBody Post newPost){
         System.out.println(newPost.getTitle());
         System.out.println(newPost.getContent());
+        postsRepository.save(newPost);
     }
     @PutMapping("/{id}")
     private void updatePost(@PathVariable Long id, @RequestBody Post post){
         System.out.println(id);
         System.out.println(post.getTitle());
         System.out.println(post.getContent());
+        postsRepository.save(post);
     }
     @DeleteMapping({"/{id}"})
     private void deletePost(@PathVariable Long id){
         System.out.println("Deleting Post with ID: " + id);
+        postsRepository.deleteById(id);
     }
 
 
