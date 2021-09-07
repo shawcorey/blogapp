e03403632020c841faac62c43cdddab44fca1f31
shawcorey <shaw.corey@gmail.com>
2021-09-06 22:37:44 -0500

    updated blogapp

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -6,6 +6,21 @@ export default function Navbar(props) {
             <a href="/about" data-link>About</a>
             <a href="/login" data-link>Login</a>
             <a href="/register" data-link>Register</a>
+            <ul class="nav nav-pills">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+  </li>
+</ul>
         </nav>
     `;
+
 }
